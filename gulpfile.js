209231ed6894d95acbb1a4cf9c27900f2efffcc1
PrestonM <pentie@gmail.com>
2018-01-30 16:27:19 +0800

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  sourcemaps = require('gulp-sourcemaps'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify');
+
+gulp.task('default', function() {
+  return gulp.src([ "src/jsmpeg.js", 
+        "src/video-element.js",
+        "src/player.js",
+        "src/buffer.js",
+        "src/ajax.js",
+        "src/ajax-progressive.js",
+        "src/websocket.js",
+        "src/source-disp.js",
+        "src/ts.js",
+        "src/decoder.js",
+        "src/json-event.js",
+        "src/mjpeg.js",
+        "src/mpeg1.js",
+        "src/mp2.js",
+        "src/webgl.js",
+        "src/canvas2d.js",
+        "src/webaudio.js",
+        "src/crc.js"])
+    // This will output the non-minified version
+    .pipe(sourcemaps.init())
+    .pipe(concat('jsmpeg.js'))
+    .pipe(gulp.dest('public'))
+    // This will minify and rename to foo.min.js",
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('public'));
+});
+
